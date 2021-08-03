5a7e655972b04134d99df2b0d4efc5dc1f647623
Philip Powers <philip_powers607@hotmail.com>
2021-08-02 20:04:28 -0400

    rounds go until all enemy robots are dead

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,11 +14,12 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+  //repeat and execute as long as the enemy-robot is alive
+  while (enemyHealth > 0) {
+    
+ 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
-
+     
     if(promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -65,6 +66,9 @@ var fight = function(enemyName) {
       window.alert("You need to pick a valid option. Try Again!");
   }
 };
+}
 for(var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
   fight(enemyNames[i]);
 }
